45d2c5c974d401fc2f350cdff52156f6bf570323
Caleb Gore <cgore2001@gmail.com>
2022-06-08 14:09:37 -0500

    chapters 16-18

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Ticket } from "./Ticket";
 import "./tickets.css";
 
 export const TicketList = ({ searchTermState }) => {
   const [tickets, setTickets] = useState([]);
+  const [employees, setEmployees] = useState([]);
   const [filteredTickets, setFilteredTickets] = useState([]);
   const [emergency, setEmergency] = useState(false);
   const [openOnly, updateOpenOnly] = useState(false);
-
+  
   const localHoneyUser = localStorage.getItem("honey_user");
   const honeyUserObject = JSON.parse(localHoneyUser);
   const navigate = useNavigate();
+  
+  const getAllTickets = () => {
+    fetch(`http://localhost:8088/serviceTickets?_embed=employeeTickets`)
+      .then((response) => response.json())
+      .then((ticketsArray) => {
+        setTickets(ticketsArray);
+      });
+  };
 
   useEffect(() => {
     const searchedTickets = tickets.filter((ticket) => {
@@ -19,7 +29,7 @@ export const TicketList = ({ searchTermState }) => {
         .startsWith(searchTermState.toLowerCase());
     });
     setFilteredTickets(searchedTickets);
-  }, [ searchTermState ]);
+  }, [searchTermState]);
 
   useEffect(() => {
     if (emergency) {
@@ -45,12 +55,16 @@ export const TicketList = ({ searchTermState }) => {
       setFilteredTickets(myTickets);
     }
   }, [openOnly]);
+
+
   useEffect(
     () => {
-      fetch(` http://localhost:8088/serviceTickets`)
+      getAllTickets();
+
+      fetch(`http://localhost:8088/employees?_expand=user`)
         .then((response) => response.json())
-        .then((ticketsArray) => {
-          setTickets(ticketsArray);
+        .then((employeeArray) => {
+          setEmployees(employeeArray);
         });
     },
     [] // When this array is empty, you are observing initial component state
@@ -97,17 +111,15 @@ export const TicketList = ({ searchTermState }) => {
       <h2>List of Tickets</h2>
 
       <article className="tickets">
-        {filteredTickets.map((ticket) => {
-          return (
-            <section className="ticket" key={`ticket--${ticket.id}`}>
-              <header>
-                <Link to={`/tickets/${ticket.id}/edit`}>Ticket {ticket.id}</Link>
-              </header>
-              <section>{ticket.description}</section>
-              <footer>Emergency: {ticket.emergency ? "🧨" : "No"}</footer>
-            </section>
-          );
-        })}
+        {filteredTickets.map((ticket) => (
+          <Ticket
+            key={`ticket--${ticket.id}`}
+            getAllTickets={getAllTickets}
+            employees={employees}
+            currentUser={honeyUserObject}
+            ticketObject={ticket}
+          />
+        ))}
       </article>
     </>
   );
